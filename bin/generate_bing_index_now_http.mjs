6deb4f504d835132cbe81bb501c2ab244d790716
Qi Liu <liuqi0611@126.com>
2024-10-23 23:18:49 +0800

    add IndexNow

---
+++ bin/generate_bing_index_now_http.mjs
@@ -0,0 +1,52 @@
+import xml2js from "xml2js";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+async function getAllLocFromSitemap(sitemapUrl) {
+  const text = await fetch(sitemapUrl).then((response) => response.text());
+  const parser = new xml2js.Parser();
+  const result = await parser.parseStringPromise(text);
+  return result.urlset.url;
+}
+
+async function generate_bing_index_now_http(site) {
+  const { sitemapURL, ...rest } = site;
+  const xml = await getAllLocFromSitemap(sitemapURL);
+  const urls = xml.map((url) => url.loc[0]);
+
+  const body = JSON.stringify(
+    {
+      ...rest,
+      urlList: urls,
+    },
+    null,
+    2
+  );
+  const raw = `POST https://api.indexnow.org/IndexNow
+Content-Type: application/json; charset=utf-8
+
+${body}
+`;
+
+  return raw;
+}
+
+const sites = [
+  {
+    host: "thoughts.latsprince.com",
+    key: "0877f051a3374eb2905e5fcac6aa8037",
+    keyLocation: "https://thoughts.latsprince.com/0877f051a3374eb2905e5fcac6aa8037.txt",
+    sitemapURL: "https://thoughts.latsprince.com/sitemap-0.xml",
+  },
+];
+
+let content = "# https://github.com/Huachao/vscode-restclient\n";
+for (const site of sites) {
+  content += "###\n"; // separate each request
+  content += await generate_bing_index_now_http(site);
+}
+
+await writeFile(
+  path.join(import.meta.dirname, "../bing_index_now.http"),
+  content
+);
